d56fc092bf4d77e3427a0edde77116e2841091ad
Stephen Choo <stephen.w.choo@gmail.com>
2024-04-07 14:41:24 +1000

    Current highest max, with refactoring for numerical constants

--- round_0/r0script.py
+++ round_0/r0script.py
@@ -16,6 +16,11 @@ POSITION_LIMITS = {
 
 PRICE_AGGRESSION = 0 # determines how aggressively we hunt for values above and below the spread
 
+THRESHOLDS = {
+	"over": -5,
+	"mid": 10
+}
+
 class Trader:
 	def get_orders(self, state: TradingState, acceptable_price: int, product: str) -> List[Order]:
 		# market taking + making based on Stanford's 2023 entry
@@ -46,7 +51,7 @@ class Trader:
 				assert(buy_amount > 0)
 				orders.append(Order(product, ask, -buy_amount))
 
-			# if at parity, buy up until we are no longer leveraged
+			# if overleveraged, buy up until we are no longer leveraged
 			if ask == acceptable_price and buying_pos < 0:
 				buy_amount = min(-vol, -buying_pos)
 				buying_pos += buy_amount
@@ -56,20 +61,25 @@ class Trader:
 		# once we exhaust all profitable sell orders, we place additional buy orders
 		# at a price acceptable to us
 		# what that price looks like will depend on our position
-		target_buy_price = min(acceptable_price - 1, lowest_buy_price + 1)
-
-		print(f"Target buy price for market making: {target_buy_price}")
 		
 		if product_position_limit - buying_pos > 0: # if we have capacity
-			if buying_pos < -5: # if we are overleveraged to sell, buy at parity for price up to neutral position
-				vol = -buying_pos
+			if buying_pos < THRESHOLDS["over"]: # if we are overleveraged to sell, buy at parity for price up to neutral position
+				target_buy_price = min(acceptable_price, lowest_buy_price + 1)
+				vol = -buying_pos + THRESHOLDS["over"]
 				orders.append(Order(product, target_buy_price, vol))
-				print(f"Market making: buying {vol} at {target_buy_price}")
+				print(f"Market making 1: buying {vol} at {target_buy_price}")
 				buying_pos += vol
-			if -5 <= buying_pos <= 5:
-				vol = -buying_pos + 5 # if we are close to neutral
-				orders.append(Order(product, target_buy_price - 1, vol))
-				print(f"Market making: buying {vol} at {target_buy_price}")
+			if THRESHOLDS["over"] <= buying_pos <= THRESHOLDS["mid"]:
+				target_buy_price = min(acceptable_price - 1, lowest_buy_price + 1)
+				vol = -buying_pos + THRESHOLDS["mid"] # if we are close to neutral
+				orders.append(Order(product, target_buy_price, vol))
+				print(f"Market making 2: buying {vol} at {target_buy_price}")
+				buying_pos += vol
+			if buying_pos >= THRESHOLDS["mid"]:
+				target_buy_price = min(acceptable_price - 3, lowest_buy_price + 1)
+				vol = product_position_limit - buying_pos
+				orders.append(Order(product, target_buy_price, vol))
+				print(f"Market making 3: buying {vol} at {target_buy_price}")
 				buying_pos += vol
 				
 		# now we sell - we reset our position
@@ -80,6 +90,7 @@ class Trader:
 			# but we are sending negative sell orders, so we negate it
 			# max we can sell is -product_position_limit - current position
 			# if current position is negative we can sell less - if positive we can sell more
+			
 			if -product_position_limit - selling_pos >= 0:
 				break
 
@@ -89,7 +100,6 @@ class Trader:
 				assert(sell_amount < 0)
 				orders.append(Order(product, bid, sell_amount))
 		
-
 			# if at parity, sell up until we are no longer leveraged
 			if bid == acceptable_price and selling_pos > 0:
 				sell_amount = max(-vol, -selling_pos)
@@ -97,7 +107,8 @@ class Trader:
 				assert(sell_amount < 0)
 				orders.append(Order(product, bid, sell_amount))
 
-		# target_sell_price = max(acceptable_price)
+		# start market making with remaining quota
+		# if selling_pos
 				
 		return orders
 	
